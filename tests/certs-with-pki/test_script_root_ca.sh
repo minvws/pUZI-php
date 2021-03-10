51e25a8f49f639d7cdea86a91b6258afce73916b
Oscar Koeroo <okoeroo@gmail.com>
2021-03-10 22:39:46 +0100

    Adding PKI CA generation mimicking UZI Server CA chain

---
+++ tests/certs-with-pki/test_script_root_ca.sh
@@ -0,0 +1,22 @@
+openssl genrsa -out root.key 4096
+openssl req -new \
+    -key root.key \
+    -subj "/C=NL/O=Fake Staat der Nederlanden/CN=Fake Staat der Nederlanden Private Root CA - G42" \
+    -nodes \
+    -set_serial 0x$(openssl rand -hex 16) \
+    -addext basicConstraints=critical,CA:TRUE \
+    -addext keyUsage=critical,keyCertSign,cRLSign \
+    -addext certificatePolicies=1.3.3.7 \
+    -addext subjectKeyIdentifier=hash \
+    -out root.csr  || exit 1
+
+openssl req -noout -text -in root.csr
+
+openssl x509 \
+    -signkey root.key \
+    -days 900 \
+    -req \
+    -in  root.csr \
+    -out root.pem
+
+openssl x509 -noout -text -in root.pem
