4ad8236d728d77378a0bf252643e7138e647a143
Oscar Koeroo <okoeroo@gmail.com>
2021-03-11 23:14:43 +0100

    Split openssl config file function in separate file. Updated all users creations to match this

---
+++ tests/certs-with-pki/test_script_support_create_config.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+# Set a default to: authenticiteitcertificaat
+CERTTYPE=${CERTTYPE:-vertrouwelijkheidcertificaat}
+
+
+cat > ${NAMESPACE}.config <<End-of-message
+[v3_uzi_zorgverlener]
+basicConstraints = critical,CA:FALSE
+End-of-message
+
+# In authenticiteitcertificaten is uitsluitend het digitalSignature bit
+# opgenomen.
+if [ "${CERTTYPE}" = "authenticiteitcertificaat" ]; then
+
+cat >> ${NAMESPACE}.config <<End-of-message
+keyUsage = critical,digitalSignature
+extendedKeyUsage = clientAuth, emailProtection, codeSigning
+End-of-message
+
+
+# In vertrouwelijkheidcertificaten zijn uitsluitend de keyEncipherment en
+# dataEncipherment bits opgenomen.
+elif [ "${CERTTYPE}" = "vertrouwelijkheidcertificaat" ]; then
+
+cat >> ${NAMESPACE}.config <<End-of-message
+keyUsage = critical,keyEncipherment,dataEncipherment
+extendedKeyUsage = emailProtection, msEFS
+End-of-message
+
+
+# In handtekeningcertificaten is uitsluitend het non-Repudiation bit op unieke
+# wijze zijn opgenomen.
+elif [ "${CERTTYPE}" = "handtekeningcertificaat" ]; then
+
+cat >> ${NAMESPACE}.config <<End-of-message
+keyUsage = critical,nonRepudiation
+extendedKeyUsage = emailProtection, codeSigning
+End-of-message
+
+# In de servercertificaten (services) zijn uitsluitend de DigitalSignatureen
+# KeyEncipherment bits opgenomen.
+elif [ "${CERTTYPE}" = "servercertificaat" ]; then
+
+cat >> ${NAMESPACE}.config <<End-of-message
+keyUsage = critical,digitalSignature,keyEncipherment
+extendedKeyUsage = clientAuth, serverAuth
+End-of-message
+
+fi
+
+
+cat >> ${NAMESPACE}.config <<End-of-message
+certificatePolicies=1.3.3.7, 2.16.528.1.1003.1.2.8.4, 2.16.528.1.1003.1.2.8.5, @polselect
+
+subjectKeyIdentifier=hash
+authorityKeyIdentifier=keyid,issuer
+
+subjectAltName = otherName:2.5.5.5;IA5STRING:2.16.528.1.1003.1.3.5.5.2-1-11111111-N-90000111-01.015-00000000
+
+[polselect]
+policyIdentifier = 2.16.528.1.1003.1.2.8.6
+CPS.1=https://example.org
+End-of-message
+
+
