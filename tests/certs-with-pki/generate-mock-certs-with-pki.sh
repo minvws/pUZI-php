aed2fcd7da8125f5a4adaa272ad9ef5dd9527051
Oscar Koeroo <okoeroo@gmail.com>
2021-03-12 19:19:01 +0100

    Created new Server certificate

--- tests/certs-with-pki/generate-mock-certs-with-pki.sh
+++ tests/certs-with-pki/generate-mock-certs-with-pki.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
+# ROOT CA
 ./test_script_root_ca.sh || exit 1
+
+# First level Intermediate CA
 ./test_script_intermediate_services_ca.sh || exit 1
 
+# Second level Intermediate CA - UZI SERVERS
 ./test_script_UZI-register_Private_Server_CA_G1.sh || exit 1
 
+# Second level Intermediate CA - UZI Zorgverlener_CA
 ./test_script_UZI-register_Zorgverlener_CA_G21.sh || exit 1
 ./test_script_UZI-register_Zorgverlener_CA_G3.sh || exit 1
 
+# Second level Intermediate CA - UZI Medewerker_niet_op_naam_CA
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21.sh || exit 1
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3.sh || exit 1
 
+# Second level Intermediate CA - UZI Medewerker_op_naam_CA
 ./test_script_UZI-register_Medewerker_op_naam_CA_G21.sh || exit 1
 ./test_script_UZI-register_Medewerker_op_naam_CA_G3.sh || exit 1
 
 
-CERTTYPE="authenticiteitcertificaat"
+# Client certificates
+export CERTTYPE="authenticiteitcertificaat"
 ./test_script_UZI-register_Medewerker_op_naam_CA_G3_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3_GENERIC_USER.sh || exit 1
@@ -23,7 +31,7 @@ CERTTYPE="authenticiteitcertificaat"
 ./test_script_UZI-register_Medewerker_op_naam_CA_G21_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Zorgverlener_CA_G3_GENERIC_USER.sh || exit 1
 
-CERTTYPE="vertrouwelijkheidcertificaat"
+export CERTTYPE="vertrouwelijkheidcertificaat"
 ./test_script_UZI-register_Medewerker_op_naam_CA_G3_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3_GENERIC_USER.sh || exit 1
@@ -31,7 +39,7 @@ CERTTYPE="vertrouwelijkheidcertificaat"
 ./test_script_UZI-register_Medewerker_op_naam_CA_G21_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Zorgverlener_CA_G3_GENERIC_USER.sh || exit 1
 
-CERTTYPE="handtekeningcertificaat"
+export CERTTYPE="handtekeningcertificaat"
 ./test_script_UZI-register_Medewerker_op_naam_CA_G3_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3_GENERIC_USER.sh || exit 1
@@ -39,9 +47,8 @@ CERTTYPE="handtekeningcertificaat"
 ./test_script_UZI-register_Medewerker_op_naam_CA_G21_GENERIC_USER.sh || exit 1
 ./test_script_UZI-register_Zorgverlener_CA_G3_GENERIC_USER.sh || exit 1
 
-
-CERTTYPE="servercertificaat"
-#TODO
+export CERTTYPE="servercertificaat"
+./test_script_UZI-register_Private_Server_CA_G1_GENERIC_HOST.sh || exit 1
 
 
 exit 0
