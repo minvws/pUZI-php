ea0e361e4ee0289ae98b6be39f536540a0418560
Oscar Koeroo <okoeroo@gmail.com>
2021-03-12 23:36:48 +0100

    Restructured generator and Zorgverlener certificate files.

--- tests/certs-with-pki/generate-mock-certs-with-pki.sh
+++ tests/certs-with-pki/generate-mock-certs-with-pki.sh
@@ -22,33 +22,37 @@
 ./test_script_UZI-register_Medewerker_op_naam_CA_G3.sh || exit 1
 
 
-# Client certificates
-export CERTTYPE="authenticiteitcertificaat"
-./test_script_UZI-register_Medewerker_op_naam_CA_G3_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Zorgverlener_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_op_naam_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Zorgverlener_CA_G3_GENERIC_USER.sh || exit 1
-
-export CERTTYPE="vertrouwelijkheidcertificaat"
-./test_script_UZI-register_Medewerker_op_naam_CA_G3_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Zorgverlener_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_op_naam_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Zorgverlener_CA_G3_GENERIC_USER.sh || exit 1
-
-export CERTTYPE="handtekeningcertificaat"
-./test_script_UZI-register_Medewerker_op_naam_CA_G3_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Zorgverlener_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Medewerker_op_naam_CA_G21_GENERIC_USER.sh || exit 1
-./test_script_UZI-register_Zorgverlener_CA_G3_GENERIC_USER.sh || exit 1
-
+# Server certificate
+export SUBJECT_ALT_NAME="host-generated.example.org"
 export CERTTYPE="servercertificaat"
 ./test_script_UZI-register_Private_Server_CA_G1_GENERIC_HOST.sh || exit 1
 
+
+# Loop the certificate types for clients
+CERTTYPES=("authenticiteitcertificaat" "vertrouwelijkheidcertificaat" "handtekeningcertificaat")
+for CERTTYPE in ${CERTTYPES[*]}; do
+     echo "CERTTYPE: ${CERTTYPE}"
+     export CERTTYPE
+
+    ./test_script_UZI-register_Medewerker_op_naam_CA_G21_GENERIC_USER.sh || exit 1
+    ./test_script_UZI-register_Medewerker_op_naam_CA_G3_GENERIC_USER.sh || exit 1
+    ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G21_GENERIC_USER.sh || exit 1
+    ./test_script_UZI-register_Medewerker_niet_op_naam_CA_G3_GENERIC_USER.sh || exit 1
+
+    # Title
+    export TITLE="Arts"
+ 
+    AANSPREEKTITLES=("Apotheker" "Arts" "Fysiotherapeut" 
+                     "Gezondheidszorgpsycholoog" "Psychotherapeut" 
+                     "Tandarts" "Verloskundige" "Verpleegkundige" 
+                     "Physician assistant" "Orthopedagoog -generalist")
+    for TITLE in ${AANSPREEKTITLES[*]}; do
+        echo "TITLE: ${TITLE}"
+        export TITLE
+
+        ./test_script_UZI-register_Zorgverlener_CA_G21_GENERIC_USER.sh || exit 1
+        ./test_script_UZI-register_Zorgverlener_CA_G3_GENERIC_USER.sh || exit 1
+    done
+done
 
 exit 0
--- tests/certs-with-pki/test_script_UZI-register_Zorgverlener_CA_G21_GENERIC_USER.sh
+++ tests/certs-with-pki/test_script_UZI-register_Zorgverlener_CA_G21_GENERIC_USER.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
+export CERTTYPE=${CERTTYPE:-vertrouwelijkheidcertificaat}
+
+export TITLE="${TITLE:-physician}"
+export SERIALNUMBER="${SERIALNUMBER:-1337}"
+export GIVENNAME="${GIVENNAME:-Jan}"
+export SURNAME="${SURNAME:-Zorg}"
+
 export I_NAMESPACE="UZI-register_Zorgverlener_CA_G21_intermediate"
-export NAMESPACE="UZI-register_Zorgverlener_CA_G21_GENERIC_USER"
+export NAMESPACE="UZI-register_Zorgverlener_CA_G21_GENERIC_USER_${SERIALNUMBER}_${TITLE}_${GIVENNAME}_${SURNAME}"
 
 
-openssl genrsa -out ${NAMESPACE}.key 4096
+openssl genrsa -out "${NAMESPACE}.key" 4096
 openssl req -new \
     -key ${NAMESPACE}.key \
-    -subj "/C=NL/O=GBIC/serialNumber=133731337/TITLE=${TITLE:-physician}/SN=${SURNAME:-Zorg}/GN=${GIVENNAME:-Jan}/CN=${GIVENNAME:-Jan} ${SURNAME:-Zorg}" \
+    -subj "/C=NL/O=GBIC/serialNumber=${SERIALNUMBER}/TITLE=${TITLE}/SN=${SURNAME}/GN=${GIVENNAME}/CN=${GIVENNAME} ${SURNAME}" \
     -nodes \
     -set_serial 0x$(openssl rand -hex 16) \
     -out ${NAMESPACE}.csr || exit 1
 
-openssl req -noout -text -in ${NAMESPACE}.csr
+openssl req -noout -text -in "${NAMESPACE}.csr"
 
-
-
-export CERTTYPE=${CERTTYPE:-vertrouwelijkheidcertificaat}
 
 # Run support script to create OpenSSL config
 ./test_script_support_create_config.sh
 
 
-
 openssl x509 -req \
-    -in ${NAMESPACE}.csr \
-    -CA ${I_NAMESPACE}.pem \
-    -CAkey ${I_NAMESPACE}.key \
+    -in "${NAMESPACE}.csr" \
+    -CA "${I_NAMESPACE}.pem" \
+    -CAkey "${I_NAMESPACE}.key" \
     -CAcreateserial \
-    -days 888 \
+    -days 898 \
     -set_serial 0x$(openssl rand -hex 16) \
     -extensions uzi_main \
-    -extfile ${NAMESPACE}.config \
-    -out ${NAMESPACE}.pem || exit 1
+    -extfile "${NAMESPACE}.config" \
+    -out "${NAMESPACE}.pem" || exit 1
 
 
-openssl x509 -noout -text -in ${NAMESPACE}.pem
+openssl x509 -noout -text -in "${NAMESPACE}.pem"
