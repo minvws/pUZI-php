9f5cc54731413bb88306306698517368da6459d5
Oscar Koeroo <okoeroo@gmail.com>
2021-03-14 12:16:42 +0100

    Moving certificate signing routines to supporting functions sourceable script

--- tests/certs-with-pki/test_script_UZI-register_Medewerker_op_naam_CA_G3.sh
+++ tests/certs-with-pki/test_script_UZI-register_Medewerker_op_naam_CA_G3.sh
@@ -18,7 +18,7 @@ openssl req -noout -subject -in "${NAMESPACE}.csr"
 
 
 cat > ${NAMESPACE}.config <<End-of-message
-[v3_intermediate_uzi]
+[uzi_main]
 basicConstraints = CA:TRUE,pathlen:0
 keyUsage = critical,keyCertSign,cRLSign
 certificatePolicies=1.3.3.7, 2.16.528.1.1003.1.2.8.4, 2.16.528.1.1003.1.2.8.5, @polselect
@@ -32,15 +32,9 @@ CPS.1=https://example.org
 End-of-message
 
 
-openssl x509 -req \
-    -in ${NAMESPACE}.csr \
-    -CA ${I_NAMESPACE}.pem \
-    -CAkey ${I_NAMESPACE}.key \
-    -CAcreateserial \
-    -days 898 \
-    -set_serial 0x$(openssl rand -hex 16) \
-    -extensions v3_intermediate_uzi \
-    -extfile ${NAMESPACE}.config \
-    -out ${NAMESPACE}.pem || exit 1
+# Assuming all variables are available
+EXTENSION_MAIN="uzi_main"
+CERT_DAYS=888
+sign_certificate
 
 display_certificate "${NAMESPACE}.pem"
