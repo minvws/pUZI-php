e25980cae65f65655fc39d8cde529f5b3a40a20d
Joshua Thijssen <jthijssen@noxlogic.nl>
2021-03-28 19:02:08 +0200

    Added exception hierarchy

--- tests/UziValidatorTest.php
+++ tests/UziValidatorTest.php
@@ -2,9 +2,12 @@
 
 namespace MinVWS\PUZI\Tests;
 
+use MinVWS\PUZI\Exceptions\UziAllowedRoleException;
+use MinVWS\PUZI\Exceptions\UziAllowedTypeException;
+use MinVWS\PUZI\Exceptions\UziCaException;
+use MinVWS\PUZI\Exceptions\UziVersionException;
 use MinVWS\PUZI\UziConstants;
 use MinVWS\PUZI\UziValidator;
-use MinVWS\PUZI\Exceptions\UziException;
 use MinVWS\PUZI\UziUser;
 use PHPUnit\Framework\TestCase;
 
@@ -20,7 +23,7 @@ final class UziValidatorTest extends TestCase
         $user = new UziUser();
         $user->setOidCa("1.2.3.4");
 
-        $this->expectException(UziException::class);
+        $this->expectException(UziCaException::class);
         $this->expectExceptionMessage("CA OID not UZI register");
 
         $validator = new UziValidator(true, [], []);
@@ -33,7 +36,7 @@ final class UziValidatorTest extends TestCase
         $user->setOidCa(UziConstants::OID_CA_CARE_PROVIDER);
         $user->setUziVersion("123");
 
-        $this->expectException(UziException::class);
+        $this->expectException(UziVersionException::class);
         $this->expectExceptionMessage("UZI version not 1");
 
         $validator = new UziValidator(true, [], []);
@@ -47,7 +50,7 @@ final class UziValidatorTest extends TestCase
         $user->setUziVersion("1");
         $user->setCardType(UziConstants::UZI_TYPE_SERVER);
 
-        $this->expectException(UziException::class);
+        $this->expectException(UziAllowedTypeException::class);
         $this->expectExceptionMessage("UZI card type not allowed");
 
         $validator = new UziValidator(true, [UziConstants::UZI_TYPE_CARE_PROVIDER], []);
@@ -62,7 +65,7 @@ final class UziValidatorTest extends TestCase
         $user->setCardType(UziConstants::UZI_TYPE_CARE_PROVIDER);
         $user->setRole(UziConstants::UZI_ROLE_DENTIST);
 
-        $this->expectException(UziException::class);
+        $this->expectException(UziAllowedRoleException::class);
         $this->expectExceptionMessage("UZI card role not allowed");
 
         $validator = new UziValidator(true, [UziConstants::UZI_TYPE_CARE_PROVIDER], [UziConstants::UZI_ROLE_NURSE]);
