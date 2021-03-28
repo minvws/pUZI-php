585ed73cfbb4abd0b4b8fc11f7053017af903a8a
Joshua Thijssen <jthijssen@noxlogic.nl>
2021-03-28 19:02:08 +0200

    Added exception hierarchy

---
+++ src/Exceptions/UziAllowedRoleException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MinVWS\PUZI\Exceptions;
+
+/**
+ * Class UziAllowedRoleException
+ * SPDX-License-Identifier: EUPL-1.2
+ * @package MinVWS\PUZI\Exceptions
+ */
+class UziAllowedRoleException extends UziException
+{
+    //
+}
---
+++ src/Exceptions/UziCaException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MinVWS\PUZI\Exceptions;
+
+/**
+ * Class UziCaException
+ * SPDX-License-Identifier: EUPL-1.2
+ * @package MinVWS\PUZI\Exceptions
+ */
+class UziCaException extends UziException
+{
+    //
+}
