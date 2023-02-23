7ccaca768bb181f5a23150ffccbdc4c20eece251
Joshua Thijssen <jthijssen@noxlogic.nl>
2023-02-23 11:15:55 +0100

    Skipping unwanted extensions (#26)
    * Skipping unwanted extensions
    
    * phpstan fix

--- src/UziReader.php
+++ src/UziReader.php
@@ -32,7 +32,7 @@ class UziReader
 
         $uziInfo = new UziUser();
 
-        // Check if the certificate is a UZI certificate
+        // Check if the certificate is an UZI certificate
         foreach ($cert['tbsCertificate']['subject']['rdnSequence'] as $sequence) {
             $data = reset($sequence);
             if ($data['type'] === 'id-at-surname') {
@@ -47,49 +47,51 @@ class UziReader
         }
 
         foreach ($cert['tbsCertificate']['extensions'] ?? [] as $extension) {
-            if ($extension['extnId'] !== "id-ce-subjectAltName") {
+            if ($extension['extnId'] === "id-ce-subjectAltName") {
+                $this->parseSubjectAltName($extension, $uziInfo);
+            }
+        }
+
+        // Only return the uzi info when we have a filled in uzi number
+        return $uziInfo->getUziNumber() ? $uziInfo : null;
+    }
+
+    protected function parseSubjectAltName(array $extension, UziUser $uziInfo): void
+    {
+        foreach ($extension['extnValue'] as $value) {
+            if (!isset($value['otherName']) || $value['otherName']['type-id'] !== UziConstants::OID_IA5STRING) {
                 continue;
             }
 
-            foreach ($extension['extnValue'] as $value) {
-                if (!isset($value['otherName']) || $value['otherName']['type-id'] !== UziConstants::OID_IA5STRING) {
-                    return null;
-                }
-
-                if (!isset($value['otherName']['value']['ia5String'])) {
-                    return null;
-                }
-
-                /**
-                 * @var array $data
-                 * Reference page 60
-                 *
-                 * [0] OID CA
-                 * [1] UZI Version
-                 * [2] UZI number
-                 * [3] Card type
-                 * [4] Subscriber number
-                 * [5] Role (reference page 89)
-                 * [6] AGB code
-                 */
-                $subjectAltName = $value['otherName']['value']['ia5String'];
-                /** @var string[]|false $data */
-                $data = explode('-', $subjectAltName);
-                if (!is_array($data) || count($data) < 6) {
-                    return null;
-                }
-                $uziInfo->setOidCa($data[0]);
-                $uziInfo->setUziVersion($data[1]);
-                $uziInfo->setUziNumber($data[2]);
-                $uziInfo->setCardType($data[3]);
-                $uziInfo->setSubscriberNumber($data[4]);
-                $uziInfo->setRole($data[5]);
-                $uziInfo->setAgbCode($data[6]);
+            if (!isset($value['otherName']['value']['ia5String'])) {
+                continue;
             }
 
-            return $uziInfo;
+            /**
+             * @var array $data
+             * Reference page 60
+             *
+             * [0] OID CA
+             * [1] UZI Version
+             * [2] UZI number
+             * [3] Card type
+             * [4] Subscriber number
+             * [5] Role (reference page 89)
+             * [6] AGB code
+             */
+            $subjectAltName = $value['otherName']['value']['ia5String'];
+            /** @var string[]|false $data */
+            $data = explode('-', $subjectAltName);
+            if (!is_array($data) || count($data) < 6) {
+                continue;
+            }
+            $uziInfo->setOidCa($data[0]);
+            $uziInfo->setUziVersion($data[1]);
+            $uziInfo->setUziNumber($data[2]);
+            $uziInfo->setCardType($data[3]);
+            $uziInfo->setSubscriberNumber($data[4]);
+            $uziInfo->setRole($data[5]);
+            $uziInfo->setAgbCode($data[6]);
         }
-
-        return null;
     }
 }
